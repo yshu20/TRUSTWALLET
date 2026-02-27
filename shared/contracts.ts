431ed56b201b236489d34b0390cab5a3cd5e2121
vamshiyogeeshwar76720 <vamshiyogeeshwar@gmail.com>
2026-02-27 16:59:21 +0530

    sepolia deployment

--- shared/contracts.ts
+++ shared/contracts.ts
@@ -356,7 +356,7 @@ export const NETWORK_TOKENS: NetworkTokens[] = [
       { symbol: "USDC", name: "Test USD Coin", address: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238", decimals: 6 },
       { symbol: "DAI", name: "Test Dai", address: "0x68194a729C2450ad26072b3D33ADaCbcef39D574", decimals: 18 },
     ],
-    subscriptionContract: "0x21668Daf33a3C38b8C670942a6B8592473fd1Cf9",
+    subscriptionContract: "0x9E2EAA6971f012d6CC1C101D4b8D9E0E5Cab8Bbf",
   },
   {
     chainId: "0x5",
